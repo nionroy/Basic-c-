a20faa341f9171c8f8f6e1d690ed666902d65433
nion-roy <nionroy18@gmail.com>
2026-06-20 12:00:15 +0600

    Add loop

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,5 +4,11 @@ using namespace std;
 int main()
 {
     cout << "Hello, World!" << endl;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Hello, World!" << endl;
+    }
     return 0;
 }
